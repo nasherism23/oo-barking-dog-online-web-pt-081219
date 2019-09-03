ac67e0ce4274cb1a16d33a0fd33148101c1d30d1
James Nash <nash.jamesm@gmail.com>
2019-09-03 03:22:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
   
   def name=(dog_name)
-    @this_dog_name = name 
+    @this_dog_name = dog_name 
   end 
   
   def name 
-    @dog_name
+    @this_dog_name
   end 
   
   def bark
